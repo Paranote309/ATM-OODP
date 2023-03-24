0e0efdfcd38af5ae51162f777948a7bd73f87b44
ParaNote <78971278+Paranote309@users.noreply.github.com>
2023-03-24 15:08:48 +0000

    #start project

---
+++ src/Decorator.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.event.*;
+
+//swing classes
+import javax.swing.text.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.border.*;
+
+public class Decorator extends JComponent {
+    public Decorator(JComponent c) {
+        setLayout(new FlowLayout());
+        // add("Center", c);
+        add(c);
+    }
+}
+// base class for implementing other decorator classes
